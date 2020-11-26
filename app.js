c306574aa02af66e65441215938ca1b295230841
Kostadin Valchev <ktvalchev@gmail.com>
2020-11-26 23:55:01 +0200

    Added Firebase to application. User can register and login, also logout

--- app.js
+++ app.js
@@ -5,12 +5,37 @@ const cookieParser = require("cookie-parser");
 const exphbs = require("express-handlebars");
 const flash = require("connect-flash");
 const session = require("express-session");
+const firebase = require("firebase");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://my-cool-dictionary.firebaseio.com",
+});
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
 
 const routes = require("./routes/index");
 const users = require("./routes/users");
 
 // Init App
 const app = express();
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
 
 // Setup body-parser Middleware
 app.use(bodyParser.json());
@@ -37,7 +62,7 @@ app.use(
 );
 
 app.use("/", routes);
-app.use("/users", users)
+app.use("/users", users);
 
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const firebase = require("firebase");
 
 // Register User
 router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log("Account successfully created");
+      res.redirect("/");
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
 });
 
 // Login User
@@ -11,9 +28,34 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log("Login successful");
+      res.redirect("/");
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      res.redirect("/login");
+    });
+});
+
 // Logout User
 router.get("/logout", (req, res) => {
-  console.log("You are now logged out");
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      console.log("Sign-out successful.");
+      res.redirect("/");
+    })
+    .catch(function (error) {
+      // An error happened.
+    });
 });
 
 module.exports = router;
