2779ac9a3048333dbd38b182717a50c6288c9a71
Kostadin Valchev <ktvalchev@gmail.com>
2021-02-03 22:13:33 +0200

    Creating dashboard

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/dashboard", (req, res) => {
+  let test = {
+    titles: ["Context", "Answer"],
+    data: [
+      {
+        contextWord: "Contextword here...",
+        answers: ["Answer1 here.."],
+      },
+    ],
+  };
+  res.render("dashboard", {
+    teableData: { titles: test.titles, data: test.data },
+  });
+});
+
 module.exports = router;
