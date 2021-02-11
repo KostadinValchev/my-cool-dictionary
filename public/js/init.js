e9d8ad3dfbcfcb9df1f38dd4dbf4cad9da372f07
Kostadin Valchev <ktvalchev@gmail.com>
2021-02-11 23:41:25 +0200

    Working on the landing page

---
+++ public/js/init.js
@@ -0,0 +1,11 @@
+(function () {
+  if (window.location.href === "http://localhost:3000/") {
+    let container = document.getElementById("main-container");
+    let errContainer = document.getElementsByClassName("error-container")[0];
+    errContainer.style.display = "none";
+    container.classList.remove("container");
+  } else {
+    let container = document.getElementById("main-container");
+    container.classList.add("container");
+  }
+})();
