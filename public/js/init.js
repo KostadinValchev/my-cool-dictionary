d50f8e966f05b5144e2fef0b9a61bd1c4ad5d240
Kostadin Valchev <ktvalchev@gmail.com>
2021-02-25 22:36:47 +0200

    Removing webpack and babel

--- public/js/init.js
+++ public/js/init.js
@@ -1,14 +1,14 @@
 (function () {
-  // if (
-  //   window.location.href === "http://localhost:3000/" ||
-  //   document.URL === "http://192.168.1.104:3000/"
-  // ) {
-  //   let container = document.getElementById("main-container");
-  //   let errContainer = document.getElementsByClassName("error-container")[0];
-  //   errContainer.style.display = "none";
-  //   container.classList.remove("container");
-  // } else {
-  //   let container = document.getElementById("main-container");
-  //   container.classList.add("container");
-  // }
+  if (
+    window.location.href === "http://localhost:3000/" ||
+    document.URL === "http://192.168.1.104:3000/"
+  ) {
+    let container = document.getElementById("main-container");
+    let errContainer = document.getElementsByClassName("error-container")[0];
+    errContainer.style.display = "none";
+    container.classList.remove("container");
+  } else {
+    let container = document.getElementById("main-container");
+    container.classList.add("container");
+  }
 })();
